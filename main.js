97f75daa530caeccbf507cb3e633ad03d9219ba6
Carlos-Zerda <czerda6@gmail.com>
2022-12-14 02:22:36 -0300

    preentrega2.1

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /** CONTROL DE VENCIMIENTOS**/
 
 class producto{
-    constructor(codigo,descripcion,cantidad,vencimiento){
+    constructor(familia,codigo,descripcion,cantidad,vencimiento){
+        this.familia=familia,
         this.codigo=codigo,
         this.descripcion=descripcion,
         this.cantidad=cantidad,
@@ -11,18 +12,35 @@ class producto{
 
 const arrayProductos=[]
 
-function crearArticulos(codigo,descripcion,cantidad,vencimiento){
-    let codigoIngreso = prompt("ingrese el codigo del producto");
-    let descripcionIngreso =prompt("ingrese la descripcion del producto");
-    let cantidadIngreso =prompt("ingrese la cantidad de unidades");
-    let vencimientoIngreso =prompt("ingrese el vencimiento");
-    return{
-        
-    }
+let inicio=parseInt(prompt("Bienvenido, por favor ingrese la opcion deseada: \n 1) Ingresar un nuevo producto \n 2) consultas"))
+
+while (inicio===1){
+    let productoNuevo = new producto( prompt("ingrese la familia a la que pertenece el producto"), prompt("ingrese el codigo del producto"),prompt("ingrese la descripcion del producto"),prompt("ingrese la cantidad de unidades"),prompt("ingrese el vencimiento"));
+    arrayProductos.push(productoNuevo);
+    alert("Producto nuevo creado!");
+    inicio=parseInt(prompt("Bienvenido, por favor ingrese la opcion deseada: \n 1) Ingresar un nuevo producto \n 2) consultas de stock"));
+}
+
+console.log(arrayProductos)
+
+while (inicio===2){
+    let consultas=parseInt(prompt("seleccione el tipo de consulta: \n 1)Existencia total \n 2) Buscar stock por familia \n 3) Buscar stock por codigo \n 4) Buscar por descripcion \n 5) Trabajar con cantidades \n 6) Trabajar con fechas \n 7) Volver al menu anterior"))
+        if(consultas===7){
+            
+            inicio=parseInt(prompt("Bienvenido, por favor ingrese la opcion deseada: \n 1) Ingresar un nuevo producto \n 2) consultas de stock"))
+            while (inicio===1){
+                let productoNuevo = new producto( prompt("ingrese la familia a la que pertenece el producto"), prompt("ingrese el codigo del producto"),prompt("ingrese la descripcion del producto"),prompt("ingrese la cantidad de unidades"),prompt("ingrese el vencimiento"));
+                arrayProductos.push(productoNuevo);
+                alert("Producto nuevo creado!");
+                inicio=parseInt(prompt("Bienvenido, por favor ingrese la opcion deseada: \n 1) Ingresar un nuevo producto \n 2) consultas de stock"));
+            }
+        }
 }
 
 
-let inicio= parseInt( prompt("Bien venido a nuestro programa de control de stock, Por favor selecciona una opcion:  \n 1) Login \n 2) Registrarse"));
+/* 
+
+let inicio= parseInt( prompt("Bienvenido a nuestro programa de control de stock, Por favor selecciona una opcion:  \n 1) Login \n 2) Registrarse"));
 
 if(inicio===2){
     let usuarioNuevo=new persona(prompt("Ingrese su nuevo Usuario"),prompt("Ingrese su Contraseña"));
@@ -42,7 +60,7 @@ else{
     }
 }
 console.log(arraypersona)
-
+ */
 
 
 
